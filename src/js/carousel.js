5b36a5477ee63d2ed09bbf16378651b08a65f428
Zhenya <fedyai.eugen@gmail.com>
2018-08-08 12:07:09 +0300

    Change slider view according to design

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -3,23 +3,16 @@ import Swiper from 'swiper';
 function init() {
   const title = document.querySelector('.title--thin span');
   const swiper = new Swiper('.swiper-container', {
-    // slidesPerView: 1,
-    // spaceBetween: 40,
-    // width: 200,
-    // centeredSlides: true,
-    // slidesOffsetBefore: 60,
     slideToClickedSlide: 'true',
-    effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: 'auto',
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
+    // slidesPerView: 1,
+    // spaceBetween: 30,
+    // freeMode: true,
+    // pagination: {
+    //   clickable: true,
+    // },
   });
 
   swiper.slideTo(2, 1000, false);
